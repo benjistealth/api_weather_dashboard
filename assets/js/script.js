7e9095c5963908e3805ed68b2e66eb8aa37e9184
Ben <stealthton@gmail.com>
2023-01-26 10:31:40 +0000

    created file structure & added starter files

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// Use the [5 Day Weather Forecast](https://openweathermap.org/forecast5)
+
+// The base URL for your API calls should look like the following: 
+// `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`
+
+var queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`;
+var latitude;
+var longitude;
+
